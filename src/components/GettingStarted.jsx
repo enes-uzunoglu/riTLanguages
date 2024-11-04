33ab9c757a325cfc32975ad9ee3f78ede1b6c80b
enes-uzunoglu <developer.enesuzunoglu@gmail.com>
2024-11-04 10:31:30 +0300

    GettigStarted component OK!

--- src/components/GettingStarted.jsx
+++ src/components/GettingStarted.jsx
@@ -1,15 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function GettingStarted () {
 
+    const [nickname, setNickname] = useState('');
+    const [readText,setReadText] = useState('');
+ 
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+
+            event.preventDefault();
+
+            const newNickname = event.target.value;
+            setNickname(newNickname);
+
+            const validasyonNickname = newNickname.replace(/[\s\d]/g, '').length > 2;
+            
+
+            if(validasyonNickname) {
+                const positiveMessage = `Let's get started! ${newNickname.trim()}`;
+                setReadText(positiveMessage);
+            } else if (newNickname === '') {
+                const nötrMessage = `You can't just enter spaces!`;
+                setReadText(nötrMessage); 
+            } else {
+                const negativeMessage = `${newNickname.trim()} is not valid!`;
+                setReadText(negativeMessage);
+            }
+
+            event.target.value = '' ;
+
+            {console.log(readText,validasyonNickname)};
+
+        }
+        
+        const bannedCharacters = ['*', '!', '@', '#', '$', '%', '^', '&', '(', ')', '-', '=', '+','_',';',':'];
+
+        if (bannedCharacters.includes(event.key)) {
+
+            event.preventDefault();
+            
+        }
+    }
+
+    
+    
+    
+
+   
+    
+    
+
+
+    const handleChange = (event) => {
+        
+    };
+    
     return(
         <>
             <form>
-                <input>
-                </input>
-                <label>
-                    Enter your nickname
+
+                <label htmlFor='exampleNickname'>
+                    Type your nickname and press Enter
                 </label>
+
+                <input
+                    id='exampleNickname'
+                    type='text'
+                    name='nickname'
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}>
+                </input>
+
+                <p>{readText}</p>
+            
             </form>
         </>
     )
